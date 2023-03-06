7b529bd0594a192e050a3a60054a5f52148ddb6f
Shawn W <78707396+sb-son@users.noreply.github.com>
2023-03-06 11:43:06 -0600

    beginning project

---
+++ src/main/java/com/codeup/sandlotconnect/SandlotConnectApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.sandlotconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandlotConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandlotConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/sandlotconnect/SandlotConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.sandlotconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandlotConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
