30a1654776913f29d05830e274fcea126e5e3edb
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-03-07 13:19:59 -0500

    completed create teams page

--- src/main/java/com/codeup/sandlotconnect/controllers/TeamController.java
+++ src/main/java/com/codeup/sandlotconnect/controllers/TeamController.java
@@ -1,30 +1,48 @@
 package com.codeup.sandlotconnect.controllers;
 
 import com.codeup.sandlotconnect.models.Team;
+import com.codeup.sandlotconnect.models.User;
+import com.codeup.sandlotconnect.repositories.TeamRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 
 @Controller
 public class TeamController {
+    private final TeamRepository teamDao;
+
+    public TeamController(TeamRepository teamDao) {
+        this.teamDao = teamDao;
+    }
 
     @GetMapping("/teams")
     public String showTeams(Model model) {
         ArrayList<Team> teams = new ArrayList<>();
-        teams.add(new Team(1, "red-dawgs", "Camden Red Dawgs Baseball", "Camden", "NJ"));
         model.addAttribute("teams", teams);
         return "teams/index";
     }
 
     @GetMapping("/teams/{id}")
-    public String showTeam() {
+    public String showTeam(@PathVariable long id, Model model) {
+        Team team = new Team(id, "Test team", "This is a test team");
+        model.addAttribute("team", team);
         return "teams/show";
     }
 
-//    @GetMapping("/teams/create")
-//    public String showCreateTeamForm() {
-//        return "teams/create";
-//    }
+    @GetMapping("/teams/create")
+    public String createTeamForm() {
+        return "teams/create";
+    }
+
+    @PostMapping("/teams/create")
+    public String createTeam(@RequestParam String name, @RequestParam String description, @RequestParam String city, @RequestParam String state, String logoUrl) {
+        Team team = new Team(name, description, city, state, logoUrl);
+        teamDao.save(team);
+        return "redirect:/teams";
+    }
 }
