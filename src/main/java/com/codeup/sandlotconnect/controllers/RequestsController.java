f50ed67f0b9e4ae734f156f42fd86de6682951a8
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-03-08 10:37:02 -0500

    changed show page

--- src/main/java/com/codeup/sandlotconnect/controllers/RequestsController.java
+++ src/main/java/com/codeup/sandlotconnect/controllers/RequestsController.java
@@ -1,8 +1,15 @@
 package com.codeup.sandlotconnect.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RequestsController {
+@PostMapping("/teams/index")
+    public String showRequest(Model model) {
 
+        return "teams/index";
+    }
 }
